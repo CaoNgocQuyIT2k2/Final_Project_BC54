38288f020a6765bb6c8b50a22e36115e6b546cc1
CaoNgocQuyIT2k2 <caongocquyit2k2@gmail.com>
2024-01-05 03:00:16 +0700

    fix app.js index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { userReducer } from './redux/reducer/reducer';
+import {applyMiddleware, combineReducers, compose, configureStore, createStore} from "@reduxjs/toolkit"
+import { spinnerReducer } from './redux/reducer/spinner';
+import thunk from 'redux-thunk'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let rootReducer = combineReducers({
+  userReducer,
+  spinnerReducer,
+});
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+);
+
+// let store = configureStore({
+//   reducer:{
+//     userReducer,
+//   }
+// })
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
